a7bec6261522d16ae547d9f069b92c937b83c942
Nick Amoscato <nick@amoscato.com>
2024-05-05 20:25:45 -0400

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import type { ReactNode } from "react";
 
@@ -10,8 +10,37 @@ const pickllleFont = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Mrs. Amoscato's Legacy",
+  title: "Mrs. Amoscato‘s Legacy",
   robots: "noindex",
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "mask-icon",
+      url: "/safari-pinned-tab.svg",
+      color: "#b23d37",
+    },
+  ],
+  manifest: "/site.webmanifest",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#fefefe",
 };
 
 interface Props {
