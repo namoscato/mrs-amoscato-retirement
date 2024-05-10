fb55faad2b0570aa29290a1459e888366b0a9d97
Nick Amoscato <nick@amoscato.com>
2024-05-10 06:25:04 -0400

    Add missing Suspense boundary

--- app/Yearbook/Yearbook.tsx
+++ app/Yearbook/Yearbook.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import clsx from "clsx";
+import { Suspense } from "react";
 import { Cover } from "../Cover";
 import styles from "./Yearbook.module.css";
 import { YearbookPage } from "./YearbookPage";
@@ -15,6 +16,22 @@ interface Props {
 }
 
 export const Yearbook = ({ students }: Props) => {
+  return (
+    <Suspense
+      fallback={
+        <YearbookStaticContent
+          students={students}
+          setActiveIndex={undefined}
+          isInactive={false}
+        />
+      }
+    >
+      <YearbookRender students={students} />
+    </Suspense>
+  );
+};
+
+const YearbookRender = ({ students }: Props) => {
   const activeIndex = useActiveIndexState({
     students,
     searchParam: STUDENT_SEARCH_PARAM,
@@ -24,20 +41,11 @@ export const Yearbook = ({ students }: Props) => {
 
   return (
     <>
-      <div className={clsx({ [styles.inactive ?? ""]: activeStudent })}>
-        <Cover />
-        <ul className={styles.list}>
-          {students.map((student, index) => (
-            <YearbookThumbnail
-              key={student.id}
-              student={student}
-              onClick={() => {
-                activeIndex.setValue(index);
-              }}
-            />
-          ))}
-        </ul>
-      </div>
+      <YearbookStaticContent
+        students={students}
+        setActiveIndex={activeIndex.setValue}
+        isInactive={!!activeStudent}
+      />
       {!!activeStudent && (
         <YearbookPage
           student={activeStudent}
@@ -47,5 +55,31 @@ export const Yearbook = ({ students }: Props) => {
         />
       )}
     </>
+  );
+};
+
+interface StaticContentProps extends Props {
+  setActiveIndex: ((index: number) => void) | undefined;
+  isInactive: boolean;
+}
+
+const YearbookStaticContent = ({
+  students,
+  setActiveIndex,
+  isInactive,
+}: StaticContentProps) => {
+  return (
+    <div className={clsx({ [styles.inactive ?? ""]: isInactive })}>
+      <Cover />
+      <ul className={styles.list}>
+        {students.map((student, index) => (
+          <YearbookThumbnail
+            key={student.id}
+            student={student}
+            onClick={setActiveIndex ? () => setActiveIndex(index) : undefined}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
